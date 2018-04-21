16ebf74458ee017c7b3a0c4f5361d937de6b8b08
eat-sleep-mine <admin@eat-sleep-mine.com>
2018-04-21 03:53:32 -0500

    further changes to meet xcn commits

--- cryptonight_dark.c
+++ cryptonight_dark.c
@@ -22,13 +22,9 @@
 // SOFTWARE.
 
 #include "crypto/oaes_lib.h"
-#include "crypto/c_keccak.h"
-#include "crypto/c_groestl.h"
-#include "crypto/c_blake256.h"
-#include "crypto/c_jh.h"
-#include "crypto/c_skein.h"
 #include "crypto/int-util.h"
 #include "crypto/hash-ops.h"
+#include "cryptonight_util.h"
 
 #define MEMORY         (1 << 19) /* 0.5 MiB */
 #define ITER           (1 << 18)
@@ -46,28 +42,6 @@ union cn_slow_hash_state {
     };
 };
 #pragma pack(pop)
-
-static void do_blake_hash(const void* input, size_t len, char* output) {
-    blake256_hash((uint8_t*)output, input, len);
-}
-
-void do_groestl_hash(const void* input, size_t len, char* output) {
-    groestl(input, len * 8, (uint8_t*)output);
-}
-
-static void do_jh_hash(const void* input, size_t len, char* output) {
-    int r = jh_hash(HASH_SIZE * 8, input, 8 * len, (uint8_t*)output);
-    assert(SUCCESS == r);
-}
-
-static void do_skein_hash(const void* input, size_t len, char* output) {
-    int r = c_skein_hash(8 * HASH_SIZE, input, 8 * len, (uint8_t*)output);
-    assert(SKEIN_SUCCESS == r);
-}
-
-static void (* const extra_hashes[4])(const void *, size_t, char *) = {
-    do_blake_hash, do_groestl_hash, do_jh_hash, do_skein_hash
-};
 
 extern int aesb_single_round(const uint8_t *in, uint8_t*out, const uint8_t *expandedKey);
 extern int aesb_pseudo_round(const uint8_t *in, uint8_t *out, const uint8_t *expandedKey);
@@ -191,7 +165,8 @@ void cryptonight_dark_hash(const char* input, char* output, uint32_t len) {
     memcpy(ctx->state.init, ctx->text, INIT_SIZE_BYTE);
     hash_permutation(&ctx->state.hs);
     /*memcpy(hash, &state, 32);*/
-    extra_hashes[ctx->state.hs.b[0] & 3](&ctx->state, 200, output);
+    //extra_hashes[ctx->state.hs.b[0] & 3](&ctx->state, 200, output);
+    do_blake_hash(&ctx->state, 200, output);
     oaes_free((OAES_CTX **) &ctx->aes_ctx);
 }
 
